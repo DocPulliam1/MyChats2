038f6a0dd83e113c8aae6d5a20523a678ebaf2c8
DocPulliam1 <deandre.pulliam@yahoo.com>
2023-11-06 22:48:51 -0800

    initial commit for adding all files

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Doc Pulliam on 21/10/2019.
+//   Copyright © 2019 Doc Pulliam. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let appName = "⚡️MyChats"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Doc Pulliam on 21/10/2019.
+//  Copyright © 2019 Doc Pulliam. All rights reserved.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = K.appName
+       
+    }
+    
+
+}
